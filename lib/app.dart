989a75ab057464460319f8df0fa7bdd94ffb7329
Rishikesh-278 <rishikeshbidkar04@gmail.com>
2023-04-04 15:36:27 +0530

    Cleaned up project

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:bloccourse/counter/counter.dart';
-
 import 'counter/view/counter_page.dart';
 
-/// {@template counter_app}
-/// A [MaterialApp] which sets the `home` to [CounterPage].
-/// {@endtemplate}
 class CounterApp extends MaterialApp {
-  /// {@macro counter_app}
   const CounterApp({super.key}) : super(home: const CounterPage());
 }
--- lib/counter/view/counter_view.dart
+++ lib/counter/view/counter_view.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:bloccourse/counter/counter.dart';
+import '../cubit/counter_cubit.dart';
 
-/// {@template counter_view}
-/// A [StatelessWidget] which reacts to the provided
-/// [CounterCubit] state and notifies it in response to user input.
-/// {@endtemplate}
 class CounterView extends StatelessWidget {
-  /// {@macro counter_view}
   const CounterView({super.key});
 
   @override
